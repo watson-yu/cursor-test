f1742478e26325c735fe63aa52f1f6e169371f11
watson-yu <watsonyu@gmail.com>
2025-06-05 09:20:50 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+/**
+ * Displays a hello world message to the console
+ */
+function displayHelloWorld(): void {
+  const message: string = "Hello, World!";
+  console.log(message);
+}
+
+// Entry point
+displayHelloWorld(); 
